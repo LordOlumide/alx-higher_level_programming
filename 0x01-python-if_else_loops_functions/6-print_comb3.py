f3cb92aca9c752374903a83540625cb1c66a8bc2
Olumide <olumideolubosede@gmail.com>
2023-03-10 19:21:55 +0000

    Ddcnid

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for a in range(0, 9):
-    for b in range(a + 1, 10):
-        print(f"{a}{b}", end=', ' if (f"{a}{b}" != "89") else '\n')
+number = 0
+while number <= 89:
+    if number % 10 == 0:
+        number += 1 + number // 10
+    print("{:02d}".format(number), end='\n' if number == 89 else ", ")
+    number += 1
