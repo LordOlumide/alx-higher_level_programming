37778b75fee9e64810939715fba8afafb25e5d18
Olumide <olumideolubosede@gmail.com>
2023-03-10 18:50:36 +0000

    Dcas

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-a = 0
-while a < 9:
-    b = a + 1
-    while b < 10:
+for a in range(0, 9):
+    for b in range(a + 1, 10):
         print(f"{a}{b}", end=', ' if (f"{a}{b}" != "89") else '\n')
-        b += 1
-    a += 1
